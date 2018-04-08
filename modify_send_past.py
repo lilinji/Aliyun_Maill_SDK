3853ccc9e25f8a68a1af33afd1ae093084ad9da1
zhoushu <zhoushu@zhoushus-MacBook-Pro.local>
2018-04-08 14:02:39 +0800

    阿里云自动邮件 SDK

--- modify_send_past.py
+++ modify_send_past.py
@@ -60,7 +60,7 @@ temp1 = '''<?php
     include_once '/opt/aliyun-php-sdk-core/Config.php';
     use Dm\Request\V20151123 as Dm;
     //需要设置对应的region名称，如华东1（杭州）设为cn-hangzhou，新加坡Region设为ap-southeast-1，澳洲Region设为ap-southeast-2。
-    $iClientProfile = DefaultProfile::getProfile("cn-hangzhou", "LTAIYHu9Sqt3hTK3", "KN30zUxIRtwz5g52Qxn8TuIx4eleEy");
+    $iClientProfile = DefaultProfile::getProfile("cn-hangzhou", "LTAIYHu9Sqt3hdR4TK3", "KN30zUxIRtwz5g52Qxn8TduIdadsx4eleEy");
     //新加坡或澳洲region需要设置服务器地址，华东1（杭州）不需要设置。
     //$iClientProfile::addEndpoint("ap-southeast-1","ap-southeast-1","Dm","dm.ap-southeast-1.aliyuncs.com");
     //$iClientProfile::addEndpoint("ap-southeast-2","ap-southeast-2","Dm","dm.ap-southeast-2.aliyuncs.com");
